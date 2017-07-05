ec062d10e58dedf54975c4d5cba3ffca1fd93ace
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-05 18:01:47 +0200

    Add debug project.

---
+++ src/Coconut.Debug/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Coconut.Debug
+{
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main (string[] args)
+        {
+            IA a = new Foo();
+            Console.WriteLine();
+            a.Muh();
+        }
+    }
+
+    public interface IA
+    {
+        void Muh ();
+    }
+
+    public class Foo : IA
+    {
+        public void Muh ()
+        {
+        }
+    }
+
+    public class Bar : IA
+    {
+        public void Muh ()
+        {
+        }
+    }
+}
